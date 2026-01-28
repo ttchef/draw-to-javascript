8fbcee06956eee7d8457fb2d9ba52581998f84b3
ttchef <ruro0.edit@gmail.com>
2026-01-28 03:51:44 -0800

    basic setup

---
+++ Makefile
@@ -0,0 +1,5 @@
+
+CC = x86_64-w64-mingw32-gcc
+
+all:
+	$(CC) main.c -o main.exe
---
+++ main.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    getc(stdin);
+    return 0;
+}
