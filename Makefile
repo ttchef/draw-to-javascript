42c314fc5ad96337afddbf91c22296b10d24066d
ttchef <ruro0.edit@gmail.com>
2026-01-28 04:35:20 -0800

    ui base

--- Makefile
+++ Makefile
@@ -6,4 +6,4 @@ CLFAGS = -Iraylib/include -I$(TINY_FILE_DIALOGS_PATH)
 LDFLAGS = -L./raylib/lib -l:libraylib.a -lwinmm -lgdi32 -lopengl32 -luser32 -lkernel32 -lcomdlg32 -lole32
 
 all:
-	$(CC) -Iraylib/include main.c $(TINY_FILE_DIALOGS_PATH)/tinyfiledialogs.c -o main.exe $(LDFLAGS)
+	$(CC) -Iraylib/include main.c ui.c $(TINY_FILE_DIALOGS_PATH)/tinyfiledialogs.c -o main.exe $(LDFLAGS)
--- main.c
+++ main.c
@@ -7,21 +7,54 @@
 #include "raygui.h"
 
 #include "libtinyfiledialogs/tinyfiledialogs.h"
+#include "ui.h"
+
+int32_t window_width = 1200;
+int32_t window_height = 800;
+
+enum uiMode {
+    UI_MODE_FILE_SELECTION,
+};
+
+void draw_ui(enum uiMode* mode) {
+    uiInfo ui_info = {
+        .start_x = window_width * 0.8f,
+        .start_y = 0,
+        .width = window_width * 0.2f,
+        .height = window_height,
+        .padding_x = ui_info.width * 0.1f,
+        .padding_y = window_height * 0.1f,
+        .element_height = window_height * 0.1f,
+    };
+    DrawRectangle(ui_info.start_x, 0, ui_info.width, window_height, DARKGRAY);
+    
+    uiBegin(&ui_info);
+
+    switch (*mode) {
+        case UI_MODE_FILE_SELECTION:
+            uiButton(NULL, "New File");
+            uiButton(NULL, "Open File");
+            break;
+    };
+
+    uiEnd(&ui_info);
+}
 
 int main() {
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(window_width, window_height, "Draw to javascirpt");
 
-    InitWindow(1200, 800, "Draw to javascirpt");
-    
+    enum uiMode mode = UI_MODE_FILE_SELECTION;
+
     while (!WindowShouldClose()) {
+        window_width = GetScreenWidth();
+        window_height = GetScreenHeight();
+
         BeginDrawing();
         ClearBackground(BLACK);
-        Rectangle bounds = {
-            .x = 20,
-            .y = 20,
-            .width = 100,
-            .height = 20,
-        };
-        GuiButton(bounds, "Button");
+
+        draw_ui(&mode);
+
         EndDrawing();
     }
 
